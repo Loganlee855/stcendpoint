874e8d2f85029a5c20b4ff48ee76f95a2445bece
Loganlee855 <hostingdrey@gmail.com>
2025-04-29 22:45:10 +0700

    new data

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -757,7 +757,7 @@ exports.GetGameLaunch = async (req, res) => {
     });
 
   } catch (err) {
-    sendError(err, "API | IntegrationService | Launch Game",req.originalUrl);
+    sendError(err, "API | IntegrationService | Launch Game",postData);
     return res.json({
       error: 1,
       description: "Internal error. Try later please",
